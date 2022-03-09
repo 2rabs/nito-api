7c8cafe5c1c4aa20b3c2814926b5bce94e769270
Ryo Takeuchi <develop@r09i.me>
2022-03-09 22:32:21 +0900

    :sparkles: Add kernel files

--- kernel/src/lib.rs
+++ kernel/src/lib.rs
@@ -1,8 +1,3 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
-}
+// src/lib.rs must be present
+pub mod model;
+pub mod repository;
